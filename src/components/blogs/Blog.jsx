b01a33a3216ba25b166c3037c77ccf3552c892b8
Mahid970 <mmahidujjaman@gmail.com>
2024-10-16 22:24:42 +0600

    completed design of blog part

--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -11,8 +11,8 @@ const Blog = ({ blog }) => {
     hashtags,
   } = blog;
   return (
-    <div className="w-2/3">
-      <img className="w-full" src={banner_img} alt="" />
+    <div className="w-2/3 my-12 flex flex-col gap-6">
+      <img className="w-full rounded" src={banner_img} alt="" />
       <div className="flex justify-between items-center">
         <div className="flex gap-4 items-center">
           <img className="w-16" src={author_img} alt="" />
@@ -26,6 +26,12 @@ const Blog = ({ blog }) => {
         </div>
       </div>
       <h1 className="text-3xl font-bold">{title}</h1>
+      <div className="flex gap-2">
+        {hashtags.map((hashtag, idx) => (
+          <p key={idx}>{hashtag}</p>
+        ))}
+      </div>
+      <hr />
     </div>
   );
 };
